b5344a7371f77507a6a2259e8932c39859bb646b
Kevin Dworzak <dworkevin@gmail.com>
2023-11-12 21:57:14 +0100

    First setup. Looking how the game should be handled.

--- Game/src/Main.java
+++ Game/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
+    private final static Game game = new Game();
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println(game.run());
     }
 }
